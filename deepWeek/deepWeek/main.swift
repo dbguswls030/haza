e7dc42d1b2e0bb4081cb32ad52dd4ca1ebee664d
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2025-03-18 14:32:30 +0900

    feat: 필수2 구현

--- deepWeek/deepWeek/main.swift
+++ deepWeek/deepWeek/main.swift
@@ -13,5 +13,10 @@ toString.calculate(num1: 3, num2: 4, closure: toString.sum)
 
 
 
+let toForEach = Problem2()
 
+toForEach.toForEach()
+print(toForEach.numbers)
 
+let result = toForEach.myMap([1,2,3,4,5])
+print(result)
