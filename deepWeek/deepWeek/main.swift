8bd1a364bbad99570d025dbb36c19fe9cae2c633
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2025-03-18 11:51:27 +0900

    feat: 필수1 구현

---
+++ deepWeek/deepWeek/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  deepWeek
+//
+//  Created by 유현진 on 3/18/25.
+//
+
+import Foundation
+
+let toString = ToString()
+print(toString.sum(1,2))
+toString.calculate(num1: 3, num2: 4, closure: toString.sum)
+
+
+
+
+
