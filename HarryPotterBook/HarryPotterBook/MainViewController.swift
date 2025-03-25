e1091f1add1d1ecb8332863df340e6a76bbc5d16
RYU HYEON JIN <69714292+dbguswls030@users.noreply.github.com>
2025-03-25 17:05:52 +0900

    feat: Level1 UI 구현

--- HarryPotterBook/HarryPotterBook/MainViewController.swift
+++ HarryPotterBook/HarryPotterBook/MainViewController.swift
@@ -6,11 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
+    private lazy var harrayPotterView: HarrayPotterView = {
+        return HarrayPotterView()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureLayout()
+        configureSeriesCollectionView()
+        configureSeriesCollectionView()
+    }
+    
+    private func configureLayout(){
+        view.backgroundColor = .systemBackground
+        view.addSubview(harrayPotterView)
+        
+        harrayPotterView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func configureSeriesCollectionView(){
+        harrayPotterView.seriesCollectionView.delegate = self
+        harrayPotterView.seriesCollectionView.dataSource = self
+    }
+}
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SeriesCollectionViewCell.identifier, for: indexPath) as? SeriesCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let cellWidth: Int = 32
+        let totalCellWidth = cellWidth * collectionView.numberOfItems(inSection: section)
+        let defaultCellSpacing = 10
+        let totalSpacing = (collectionView.numberOfItems(inSection: section) - 1) * defaultCellSpacing
+        let horizontalInset = max((collectionView.frame.width - CGFloat(totalCellWidth) - CGFloat(totalSpacing))  / 2, 0) // 음수 방지
+        return UIEdgeInsets(top: 0, left: horizontalInset, bottom: 0, right: horizontalInset)
     }
 }
